d2467c86c15cf878ec681dd4c13c870ee1478cb7
Hritam Kar <94963173+the-devil512@users.noreply.github.com>
2022-10-29 12:23:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const music_list = [
     {
         img : 'images/fallingdown.jpg',
         name : 'Falling Down',
-        artist : 'Wild Cards',
+        artist : 'Wid Cards',
         music : 'music/fallingdown.mp3'
     },
     {
